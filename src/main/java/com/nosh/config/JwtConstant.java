d5ad86398cb8268d0ab9cd54d7e6b88bf038a27d
Jeethan_crasta <jeethancs@gmail.com>
2024-09-12 18:16:34 +0530

    Initial commit

---
+++ src/main/java/com/nosh/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.nosh.config;
+
+public class JwtConstant {
+  public static final String SECRET_KEY="ZGHTSUEBBADYSBDLFPFJDMESSJDUHDHDYEDBDBDCHDHDBXVS";
+
+  public static final String JWT_HEADER="Authorization";
+}
