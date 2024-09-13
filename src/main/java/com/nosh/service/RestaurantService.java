32efb0fe32f53278f7bef2c7f617ed756799f0cf
Jeethan_crasta <jeethancs@gmail.com>
2024-09-13 22:24:59 +0530

    restaurant  controller

--- src/main/java/com/nosh/service/RestaurantService.java
+++ src/main/java/com/nosh/service/RestaurantService.java
@@ -17,13 +17,13 @@ public interface RestaurantService {
 
     public List<Restaurant> getAllRestaurants();
 
-    public List<Restaurant> SearchRestaurant(String keyword);
+    public List<Restaurant> searchRestaurant(String keyword);
 
     public Restaurant findRestaurantById(Long id) throws Exception;
 
     public Restaurant getRestaurantByUserId(Long userId) throws Exception;
 
-    public RestaurantDto addToFavorite(Long restaurantId,User user) throws Exception;
+    public RestaurantDto addToFavorites(Long restaurantId,User user) throws Exception;
 
     public Restaurant updateRestaurantStatus(Long id) throws Exception;
 
